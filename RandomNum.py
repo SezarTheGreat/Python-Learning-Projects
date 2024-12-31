f2edbd030bee5504d2193c0668f83d52d9b674ff
Jyotishman Barman <jyotishmanjbbarman@gmail.com>
2024-12-31 16:55:28 +0530

    Updated Random game

--- RandomNum.py
+++ RandomNum.py
@@ -1,21 +1,36 @@
+#Python number guessing game using random module
 import random
 
-low = int(input("Enter a low range: "))
-high = int(input("Enter a high range: "))
+low = 1
+high = 100
 guesses = 0
 
 number = random.randint(low,high)
+is_Running = True
 
-while True:
-    guess = int(input(f"Enter a number between ({low} to {high}): "))
-    guesses +=1
+print("PYTHON NUMBER GUESSING GAME")
+if_True = input("Do you want to play this game (Y/N): ")
 
-    if guess < number:
-        print(f"{guess} is too low.")
-    elif guess > number:
-        print(f"{guess} is too high.")
-    else:
-        print(f"{guess} is correct.")
-        break
-
-print(f"This round took you {guesses} guesses.")
+if if_True.lower() == 'y':
+    while is_Running:
+        guess =  input(f"Enter your guess between {low} and {high}: ")
+        if guess.isdigit():
+            guess = int(guess)
+            guesses +=1
+            
+            if guess < low or guess > high:
+                print("That number is out of range.")
+                print(f"Please select a number between {low} and {high}: ")
+            elif guess < number:
+                print("Too low! Try again!")
+            elif guess > number:
+                print("Too High! Try Again!")
+            else:
+                print(f"Correct guess! The answer was {number}")
+                print(f"Number of guesses: {guesses}")
+                is_Running = False
+        else:
+            print("Invalid Guess.")
+            print(f"Please select a number between {low} and {high}: ")
+else:
+    print("OK :((")
